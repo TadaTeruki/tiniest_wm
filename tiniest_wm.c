a1a8c69150ef6069461b1414ac9c4cdce3be6bbb
TadaTeruki <tadateruki.public@gmail.com>
2022-01-12 00:43:49 +0900

    improve window motion

--- tiniest_wm.c
+++ tiniest_wm.c
@@ -1,4 +1,4 @@
 //You just DO WHAT THE FUCK YOU WANT TO.
 #include<X11/Xlib.h>
 #define V e.xbutton
-int main(){Display*d;XEvent e;size_t w,p=GrabModeAsync;if(d=XOpenDisplay(0)){XGrabButton(d,1,Mod1Mask,XRootWindow(d,0),1,ButtonPressMask,p,p,0,0);for(;;){XNextEvent(d,&e);if(w=V.subwindow){XMoveWindow(d,w,V.x-9,V.y-9);}}}}
+int main(){Display*d;XEvent e;Window w,p=GrabModeAsync;if(d=XOpenDisplay(0)){XGrabButton(d,1,Mod1Mask,XRootWindow(d,0),1,ButtonPressMask,p,p,0,0);for(;;){XNextEvent(d,&e);if(w=V.subwindow){XWindowAttributes a;XGetWindowAttributes(d,w,&a);XMoveWindow(d,w,V.x-a.width/2,V.y-a.height/2);}}}}
